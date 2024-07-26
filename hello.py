787bb39bd8b5db427a85b8a894d5f2af6d98c876
ducmanh1801 <ducmanh1801@gmail,com>
2024-07-26 17:01:11 +0700

    change hello file

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("Hello")
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print(self.name, self.age)
+
+class Student(Person):
+    def __init__(self, name, age):
+        self.Sname = name
+        self.Sage = age
+        super().__init__('Manh',age)
+    def displayS(self):
+        print(self.Sname, self.Sage)
+
+obj = Student('Hieu', 23)
+obj.display()
+obj.displayS()
